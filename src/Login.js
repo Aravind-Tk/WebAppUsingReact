cfe6098061c33612fbea11b3ff8c3934973f2b58
aravind krishnan <aravindkrishnan567@gmail.com>
2023-03-30 22:43:16 +0530

    new login v1

--- src/Login.js
+++ src/Login.js
@@ -1,12 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './Header/Header';
-
+import FloatingInput from "../src/inputControls/floatingInput/FloatingInput"
 
  const Login = () => {
+
+  const [email,setEmail] = useState("");
+  
+  const handleInputChange = (text) => {
+    debugger;
+   setEmail(text)
+  
+  };
   return (
     <>
     <Header/>
     <div>Login</div>
+    <FloatingInput 
+       type="email"
+       label="E-mail"
+       htmlFor="email"
+       maxLength={37}
+       value={email}
+       handleChange={(text) => {
+         handleInputChange(text);
+       }}
+       />
+   
+    <FloatingInput 
+    
+  class= "Password"
+    
+    
+    
+    
+    />
+
 
     </>
   );
